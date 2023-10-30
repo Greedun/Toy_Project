b20d3de316c48135c2c421d903d839c0af1d161c
greedun <fogreedonr1405@gmail.com>
2023-10-30 10:06:10 +0900

    modify .gitignore and delete data folder

--- DH_scraping/lecture_crawling.py
+++ DH_scraping/lecture_crawling.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+import time,sys
 
 base_path = 'https://dreamhack.io/lecture/courses'
 
-length = 700
+length = 500
 total_result = ['' for j in range(length)]
 
-for i in range(length):
+for i in range(1,length):
     if i%10 ==0:
         print(f"{i} 진행중")
 
@@ -17,17 +17,20 @@ for i in range(length):
     r = requests.get(target_path)
     #print(r.status_code, r.text) 
     result = r.text
+    #print(result)
+
     if 'error-search-no-result' in result: # X
         continue
     else:
         html = r.text
+        #print(html)
         soup = BeautifulSoup(html, 'html.parser')
+        title = soup.select_one('#course-title > div.content > div.course-title')
+        title = str(title).split(">")[1].split("<")[0].replace("\n","")
 
-        title = soup.select_one('#__layout > div > main > section > section > header > div > div.information > h2 > span')
-        title = str(title).split(">")[1].replace('</span>','').replace('</span','')
         total_result[i] = title
     #print(lecture_num,title)
-    time.sleep(0.1)
+    time.sleep(0.05)
 
 #print(total_result)
 
